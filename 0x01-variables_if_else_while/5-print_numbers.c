be1296ae540dc4456e740291a9cf9f7d05505017
root <chijokeogbuanya@gmail.com>
2022-03-11 11:20:03 -0800

    correct

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,8 +9,8 @@ int main(void)
 {
    int digit;
 
-   for (digit = 0; digit < 10; digit++)
+	for (digit = 0; digit < 10; digit++)
 	printf("%i", digit);
-	 putchar('\n');
-   	  return (0);
+	putchar('\n');
+	return (0);
 }
