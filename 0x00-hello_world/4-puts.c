af5937d5e0107637e65540c20f362deb7a4ea10c
root <chijokeogbuanya@gmail.com>
2022-03-10 23:30:45 -0800

    go

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main- wher the c program starts
+ * return: 0
+ **/
+
+  main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+		return (0);
+
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+Playing with C for the first time.
