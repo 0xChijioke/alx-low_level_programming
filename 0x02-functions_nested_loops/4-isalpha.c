984cc1fab93ba4f823fca7c6e92c1f05cda636b2
root <chijokeogbuanya@gmail.com>
2022-03-15 06:08:02 -0700

    correct

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,6 @@
  * _isalpha - checks if a character is an alphabet
  *
  * @c: the character to be checked
- * 
  * Return: returns 1 if the character is an alphabet and 0 otherwise
  */
 int _isalpha(int c)
