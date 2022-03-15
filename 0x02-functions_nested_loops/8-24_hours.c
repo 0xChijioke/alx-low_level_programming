253c724c8e55b1777aa93196474217aebbc022cb
root <chijokeogbuanya@gmail.com>
2022-03-15 07:12:07 -0700

    fix for betty

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *jack_bauer - prints every minute of the day starting from 00:00 to 23:59
- *
- * Return: returns 0
- */
+  *jack_bauer - prints every minute of the day starting from 00:00 to 23:59
+  *
+  * Return: returns 0
+  */
 void jack_bauer(void)
 {
 	int hr;
