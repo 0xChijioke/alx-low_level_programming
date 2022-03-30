3f993acbd48dc0f30e6657ede90ebfe3d001aafd
root <chijokeogbuanya@gmail.com>
2022-03-30 04:27:46 -0700

    correction

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,7 +5,7 @@ int _sqrt(int n, int i);
 
 /**
  * _sqrt_recursion - Returns the natural square root of a number
- *@bn number to calculate the natural square root
+ *@n: number to calculate the natural square root
  *
  * Return: the natural square root
  */
